725715c5d93522ecf164c7536523a9c120a98b81
yanghan011023 <18569452675>
2021-09-29 12:22:17 +0800

    修改

--- src/com/yh/test/LazySingleton.java
+++ src/com/yh/test/LazySingleton.java
@@ -4,14 +4,14 @@ package com.yh.test;
  * 单例模式-懒汉模式
  */
 public class LazySingleton {
-    private LazySingleton(){
+    private LazySingleton() {
         System.out.println("单例懒汉模式----");
     }
 
     private static LazySingleton lazySingleton = null;
 
-    public static LazySingleton getLazySingletonInstance(){
-        if (lazySingleton==null){
+    public static LazySingleton getLazySingletonInstance() {
+        if (lazySingleton == null) {
             lazySingleton = new LazySingleton();
         }
         return lazySingleton;
